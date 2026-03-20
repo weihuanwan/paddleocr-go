f5263621752610cc9b799d6f9e26c6595e23751c
万伟焕 <18776625633@163.com>
2026-03-20 18:05:42 +0800

    版面区域检测模块

--- ocr/DocLayoutPlusLSession.go
+++ ocr/DocLayoutPlusLSession.go
@@ -14,64 +14,108 @@ import (
  */
 type DocLayoutPlusLSession struct {
 	OnnxSession *ort.DynamicAdvancedSession
-	config      *PaddleOCRConfig
+	Alpha       [3]float32
+	Beta        [3]float32
+}
+
+func NewDocLayoutPlusLSession(onnxSession *ort.DynamicAdvancedSession) *DocLayoutPlusLSession {
+	scale := float32(1.0 / 255.0)
+	mean := []float32{0, 0, 0}
+	std := []float32{1, 1, 1}
+	var alpha [3]float32
+	var beta [3]float32
+
+	for i := 0; i < 3; i++ {
+		alpha[i] = scale / std[i]
+		beta[i] = -mean[i] / std[i]
+	}
+
+	return &DocLayoutPlusLSession{
+		onnxSession,
+		alpha,
+		beta,
+	}
 }
 
 func (docLayout *DocLayoutPlusLSession) Run(originImage *gocv.Mat) ([]*DetResult, error) {
 	// 缩放
-	resizedImage, _, err := docLayout.resize(originImage)
+	resizedImage, scale, err := docLayout.resize(originImage)
 	if err != nil {
 		return nil, err
 	}
 
 	defer resizedImage.Close()
-	// 归一化
-	imageNormalize, err := common.Normalize(resizedImage, [3]float32{1, 2, 3}, [3]float32{1, 2, 3})
+	// 归一化 # [0.00392156862745098, 0.00392156862745098, 0.00392156862745098]
+	//# [-0.0, -0.0, -0.0]
+	imageNormalize, err := common.Normalize(resizedImage, docLayout.Alpha, docLayout.Beta)
 
 	if err != nil {
 		return nil, err
 	}
 
 	defer imageNormalize.Close()
-	//w := gocv.NewWindow("image")
-	//w.ResizeWindow(imageNormalize.Cols(), imageNormalize.Rows())
-	//w.IMShow(*imageNormalize)
-	//w.WaitKey(0)
+
 	// 转换 chw
 	imageCHW := common.HWCToCHW(imageNormalize)
 
-	// 描述
-	shape := ort.NewShape(1, 3, int64(resizedImage.Rows()), int64(resizedImage.Cols()))
-	// 创建张量
-	detInputTensor, err := ort.NewTensor(shape, imageCHW)
+	// 1.
+	imageTensor, err := ort.NewTensor(ort.NewShape(1, 2), []float32{float32(resizedImage.Rows()), float32(resizedImage.Cols())})
 	if err != nil {
-
-		return nil, fmt.Errorf("create rec input tensor error", err.Error())
+		return nil, fmt.Errorf("imageTensor input tensor error", err.Error())
 	}
-	defer detInputTensor.Destroy()
+	defer imageTensor.Destroy()
 
-	detOutputShape := ort.NewShape(1, 1, int64(resizedImage.Rows()), int64(resizedImage.Cols()))
-	detOutputTensor, err := ort.NewEmptyTensor[float32](detOutputShape)
+	// 2.
+	dataTensor, err := ort.NewTensor(ort.NewShape(1, 3, int64(resizedImage.Rows()), int64(resizedImage.Cols())), imageCHW)
+	if err != nil {
+		return nil, fmt.Errorf("dataTensor input tensor error", err.Error())
+	}
+	defer dataTensor.Destroy()
+	//]float32{0.17821342, 0.25188917}
+	scaleFactorTensor, err := ort.NewTensor(ort.NewShape(1, 2), scale)
+	if err != nil {
+		return nil, fmt.Errorf("dataTensor input tensor error", err.Error())
+	}
+	defer scaleFactorTensor.Destroy()
+
+	output0Tensor, err := ort.NewEmptyTensor[float32](ort.NewShape(300, 6))
 
 	if err != nil {
-		return nil, fmt.Errorf("create det output tensor error", err.Error())
+		return nil, fmt.Errorf("output0Tensor output tensor error", err.Error())
 	}
-	defer detOutputTensor.Destroy()
+
+	defer output0Tensor.Destroy()
+
+	output1Tensor, err := ort.NewEmptyTensor[int32](ort.NewShape(1))
+
+	if err != nil {
+		return nil, fmt.Errorf("output0Tensor output tensor error", err.Error())
+	}
+
+	defer output1Tensor.Destroy()
 	// 检测（核心）
-	err = docLayout.OnnxSession.Run([]ort.Value{detInputTensor}, []ort.Value{detOutputTensor})
+	err = docLayout.OnnxSession.Run([]ort.Value{imageTensor, dataTensor, scaleFactorTensor}, []ort.Value{output0Tensor, output1Tensor})
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
 
 	return nil, err
 }
 
 // 缩放图片
-func (docLayout *DocLayoutPlusLSession) resize(imageMat *gocv.Mat) (*gocv.Mat, []int, error) {
+func (docLayout *DocLayoutPlusLSession) resize(imageMat *gocv.Mat) (*gocv.Mat, []float32, error) {
 
 	resizeMat := gocv.NewMat()
+
 	err := gocv.Resize(*imageMat, &resizeMat, image.Pt(800, 800), 0, 0, gocv.InterpolationCubic)
 
 	if err != nil {
 		return nil, nil, fmt.Errorf("DocLayoutPlusLSession resize failed: %v", err)
 	}
-	return &resizeMat, make([]int, 0), nil
+
+	scaleW := float32(800) / float32(imageMat.Cols())
+	scaleH := float32(800) / float32(imageMat.Rows())
+
+	return &resizeMat, []float32{scaleH, scaleW}, nil
 
 }
---
+++ ocr/PPStructureSession.go
@@ -0,0 +1,108 @@
+package ocr
+
+import (
+	"fmt"
+
+	"github.com/weihuanwan/paddleocr-go/common"
+	ort "github.com/yalue/onnxruntime_go"
+)
+
+type PPStructureSession struct {
+	DocLayoutSession *DocLayoutPlusLSession // 方向
+}
+
+/*
+*
+ppocr 配置
+*/
+type PPStructureOCRConfig struct {
+	*PaddleOCRConfig
+	DocLayoutPlusLModelPath string
+}
+
+func NewDefaultPPStructureOCRConfig(
+	onnxRuntimeLibPath string,
+	docLayoutPlusLModelPath string,
+
+) *PPStructureOCRConfig {
+
+	paddleOCRConfig := NewDefaultPaddleOCRConfig(
+		onnxRuntimeLibPath,
+		"./test/model/det.onnx",
+		"./test/model/rec.onnx",
+		"./test/model/cls.onnx",
+		"./test/model/dict.txt",
+		false,
+		true,
+	)
+	config := &PPStructureOCRConfig{
+		PaddleOCRConfig:         paddleOCRConfig,
+		DocLayoutPlusLModelPath: docLayoutPlusLModelPath,
+	}
+
+	return config
+}
+func NewPPStructureSession(config *PPStructureOCRConfig) (*PPStructureSession, error) {
+
+	// ✅ 基础校验
+	if config.OnnxRuntimeLibPath == "" {
+		return nil, fmt.Errorf("OnnxRuntimeLibPath is required")
+	}
+
+	// ✅ 初始化 ONNX（只执行一次）
+	if err := initOrt(config.OnnxRuntimeLibPath); err != nil {
+		return nil, fmt.Errorf("initialize ONNX Runtime: %w", err)
+	}
+
+	// ✅ session options
+	options, err := ort.NewSessionOptions()
+	if err != nil {
+		return nil, fmt.Errorf("create session options: %w", err)
+	}
+	defer options.Destroy()
+
+	if config.NumThreads > 0 {
+		if err := options.SetIntraOpNumThreads(config.NumThreads); err != nil {
+			return nil, fmt.Errorf("set intra-op threads: %w", err)
+		}
+	}
+	// ✅ CUDA
+	if config.UseCuda {
+		cudaOptions, err := ort.NewCUDAProviderOptions()
+		if err != nil {
+			return nil, fmt.Errorf("create CUDA provider options: %w", err)
+		}
+		defer cudaOptions.Destroy()
+
+		if err := options.AppendExecutionProviderCUDA(cudaOptions); err != nil {
+			return nil, fmt.Errorf("append CUDA provider: %w", err)
+		}
+	}
+
+	// CLS
+	docLayoutPlusLSessionInternal, err := ort.NewDynamicAdvancedSession(
+		config.DocLayoutPlusLModelPath,
+		[]string{"im_shape", "image", "scale_factor"},
+		[]string{"fetch_name_0", "fetch_name_1"},
+		options,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("create classification session: %w", err)
+	}
+	docLayoutSession := NewDocLayoutPlusLSession(docLayoutPlusLSessionInternal)
+	// 构建 session
+	session := &PPStructureSession{
+		DocLayoutSession: docLayoutSession,
+	}
+
+	return session, nil
+}
+func (session *PPStructureSession) RunOCR(imagePath string) {
+	imageMat, _, err := common.LoadImage(imagePath)
+
+	if err != nil {
+
+	}
+
+	session.DocLayoutSession.Run(imageMat)
+}
